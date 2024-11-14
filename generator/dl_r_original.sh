6be6f093a0f19be11d59cdf88d3282c1914814fb
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-11-14 19:43:04 +0000

    regenerate for original in refinement exp

---
+++ generator/dl_r_original.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python ./refinement_test.py bm25 dl_19 original 3
+echo 'Finished generation for 3shot for trec-dl-19 o bm25!'
+python ./refinement_test.py mt5 dl_19 original 3
+echo 'Finished generation for 3shot for trec-dl-19 o mt5!'
+python ./refinement_test.py bm25 dl_20 original 3
+echo 'Finished generation for 3shot for trec-dl-20 o bm25!'
+python ./refinement_test.py mt5 dl_20 original 3
+echo 'Finished generation for 3shot for trec-dl-20 o mt5!'
