c9edbc67265e0208d20ed262d60ea3b9bb3ceae6
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-11-18 16:32:27 +0000

    evaluations for 5 7 10

---
+++ evaluator/dl_19_and_20_s-rerank_exp_evals_5.sh
@@ -0,0 +1,12 @@
+python evaluation_of_answers.py 5 5 dl_19 bm25 integrated_original
+python evaluation_of_answers.py 5 5 dl_20 bm25 integrated_original
+python evaluation_of_answers.py 5 5 dl_19 bm25 integrated_s-reranked
+python evaluation_of_answers.py 5 5 dl_20 bm25 integrated_s-reranked
+python evaluation_of_answers.py 5 5 dl_19 bm25 integrated_s-reranked_position
+python evaluation_of_answers.py 5 5 dl_20 bm25 integrated_s-reranked_position
+python evaluation_of_answers.py 5 5 dl_19 mt5 integrated_original
+python evaluation_of_answers.py 5 5 dl_20 mt5 integrated_original
+python evaluation_of_answers.py 5 5 dl_19 mt5 integrated_s-reranked
+python evaluation_of_answers.py 5 5 dl_20 mt5 integrated_s-reranked
+python evaluation_of_answers.py 5 5 dl_19 mt5 integrated_s-reranked_position
+python evaluation_of_answers.py 5 5 dl_20 mt5 integrated_s-reranked_position
