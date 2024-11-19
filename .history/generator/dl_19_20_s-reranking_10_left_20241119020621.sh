a54ebc9f92030bd75f4ef4600c0e363edc8dbe73
FangzhengAndrewTian <2612785T@student.gla.ac.uk>
2024-11-19 02:07:57 +0000

    k=10 tests left someones

---
+++ .history/generator/dl_19_20_s-reranking_10_left_20241119020621.sh
@@ -0,0 +1,2 @@
+python ./sentence_reranking_test.py mt5 dl_19 s-reranked 10
+python ./sentence_reranking_test.py mt5 dl_19 s-reranked_position 10
---
+++ .history/generator/dl_19_20_s-reranking_10_left_20241119020700.sh
@@ -0,0 +1,4 @@
+python ./sentence_reranking_test.py mt5 dl_19 s-reranked 10
+python ./sentence_reranking_test.py mt5 dl_19 s-reranked_position 10
+python ./sentence_reranking_test.py bm25 dl_20 s-reranked 10
+python ./sentence_reranking_test.py bm25 dl_20 s-reranked_position 10
